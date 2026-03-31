1fc2be839f089f0062f942557788a25204986e11
rbenedict-debug <rbenedict@k12insight.com>
2026-03-31 12:39:43 -0500

    feat(paginator): wire to MatPaginatorModule

--- components/paginator/paginator.component.ts
+++ components/paginator/paginator.component.ts
@@ -14,7 +14,7 @@
  */
 
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 export interface DsPageEvent {
   pageIndex: number;
@@ -25,7 +25,7 @@ export interface DsPageEvent {
 @Component({
   selector: 'ds-paginator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatPaginatorModule],
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -38,33 +38,7 @@ export class DsPaginatorComponent {
   @Input() showFirstLastButtons = true;
   @Output() pageChange = new EventEmitter<DsPageEvent>();
 
-  get totalPages(): number {
-    return Math.ceil(this.total / this.pageSize);
-  }
-
-  get rangeStart(): number {
-    return this.pageIndex * this.pageSize + 1;
-  }
-
-  get rangeEnd(): number {
-    return Math.min((this.pageIndex + 1) * this.pageSize, this.total);
-  }
-
-  get isFirstPage(): boolean { return this.pageIndex === 0; }
-  get isLastPage(): boolean  { return this.pageIndex >= this.totalPages - 1; }
-
-  private emit(): void {
-    this.pageChange.emit({ pageIndex: this.pageIndex, pageSize: this.pageSize, length: this.total });
-  }
-
-  firstPage(): void  { this.pageIndex = 0; this.emit(); }
-  prevPage(): void   { if (!this.isFirstPage) { this.pageIndex--; this.emit(); } }
-  nextPage(): void   { if (!this.isLastPage) { this.pageIndex++; this.emit(); } }
-  lastPage(): void   { this.pageIndex = this.totalPages - 1; this.emit(); }
-
-  onPageSizeChange(size: number): void {
-    this.pageSize = size;
-    this.pageIndex = 0;
-    this.emit();
+  onPage(event: PageEvent): void {
+    this.pageChange.emit({ pageIndex: event.pageIndex, pageSize: event.pageSize, length: event.length });
   }
 }
